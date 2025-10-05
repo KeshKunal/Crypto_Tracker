91f8d42892c4f4ad607e5ef7e9ba6773980dcda3
KeshKunal <infini2ude@gmail.com>
2025-10-05 18:10:24 +0530

    blueprint for single coin

---
+++ src/main/java/com/example/Coin.java
@@ -0,0 +1,69 @@
+package com.example;
+
+import javafx.beans.property.SimpleDoubleProperty;
+import javafx.beans.property.SimpleStringProperty;
+
+public class Coin {
+
+    private final SimpleStringProperty name;
+    private final SimpleDoubleProperty price;
+    private final SimpleDoubleProperty holdings;
+    private final SimpleDoubleProperty value;
+
+    public Coin(String name, double price, double holdings)
+    {
+        this.name = new SimpleStringProperty(name);
+        this.price = new SimpleDoubleProperty(price);
+        this.holdings = new SimpleDoubleProperty(holdings);
+        this.value = new SimpleDoubleProperty(price*holdings);
+
+    }
+    
+    // Getters and property method
+
+    public String getName()
+    {
+        return name.get();
+    }
+
+    public SimpleStringProperty nameProperty()
+    {
+        return name;
+    }
+
+    public double getPrice()
+    {
+        return price.get();
+    }
+
+    public SimpleDoubleProperty priceProperty()
+    {
+        return price;
+    }
+
+    public void setPrice(double newPrice)
+    {
+        this.price.set(newPrice);
+        this.value.set(newPrice * getHoldings());
+    }
+
+    public double getHoldings()
+    {
+        return holdings.get();
+    }
+
+    public SimpleDoubleProperty holdingsProperty()
+    {
+        return holdings;
+    }
+
+    public double getValue()
+    {
+        return value.get();
+    }
+
+    public SimpleDoubleProperty valueProperty()
+    {
+        return value;
+    }
+}
